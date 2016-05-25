ec9913041f5893a687aaa1818d9070391deb3dad
Pattrick Muller Queiroga <pattrickmiller@gmail.com>
2016-05-24 23:29:57 -0300

    package.json include start node app

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -22,6 +22,17 @@ function createDBConnection(){
 
 	};
 
+	if (process.env.NODE_ENV == 'production') {
+        var url = process.env.CLEARDB_DATABASE_URL;
+        var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+        return mysql.createConnection({
+            host:grupos[3],
+            user:grupos[1],
+            password:grupos[2],
+            database:grupos[4]
+        });
+    }
+
 };
 
 module.exports = function(){
